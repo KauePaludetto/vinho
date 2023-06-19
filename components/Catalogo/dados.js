555a2f60ef3125debae7f7133e856ff392af90aa
KauePaludetto <kauepaludetto15@gmail.com>
2023-06-19 14:02:06 -0300

    Final

---
+++ components/Catalogo/dados.js
@@ -0,0 +1,40 @@
+const vinhos = [
+        {
+            "id": 1,
+            "nome":"Chatigny Chardonna",
+            "descricao": "Vinho leve, refrescante e levemente cítrico da cor amarelo palha. Perfeito com carnes brancas e massa ao pesto.",
+            "preco": "R$ 100,00",
+            "fotos": [
+                "vinho-branco.png",
+            ]
+        },
+        {
+            "id": 2,
+            "nome":"Concha y Toro Exportacion",
+            "descricao": "Vinho rosé fresco, intenso e macio da cor rosa pálido. Perfeito com saladas e aperitivos.",
+            "preco": "R$ 60,00",
+            "fotos": [
+                "vinho-rose.png",
+            ]
+        },
+        {
+            "id": 3,
+            "nome":"Portada Winemaker's",
+            "descricao": "Vinho encorpado, saboroso e frutado, com final levemente adocicado. Sua cor é vermelho-rubi.Perfeito com queijo parmesão e carnes assadas ou grelhadas.",
+            "preco": "R$ 50,00",
+            "fotos": [
+                "vinho-seco.png",
+            ]
+        },
+        {
+            "id": 4,
+            "nome":"Elvio Cogno Ravera Barolo",
+            "descricao": "Vinho estruturado, com sabor de cereja vermelha madura, framboesa, notas de tabaco e taninos aveludados. Sua cor é vermelho granada e é perfeito com carnes vermelhas e molhos encorpados.",
+            "preco": "R$ 800,00",
+            "fotos": [
+                "vinho-especial.png",
+            ]
+        },
+        
+    ];
+export default vinhos;
